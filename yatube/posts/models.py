f6e6ef1dbe45c1e5b00786cb1f993469c77f032c
Ridesiberia <119219467+Ridesiberia@users.noreply.github.com>
2023-02-28 19:58:01 +0700

    исправленная версия после второго ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -22,18 +22,21 @@ class Post(models.Model):
     text = models.TextField(verbose_name='Текст поста')
     pub_date = models.DateTimeField(
         auto_now_add=True,
-        verbose_name='Дата публикации')
+        verbose_name='Дата публикации',
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор')
+        verbose_name='Автор',
+    )
     group = models.ForeignKey(
         'Group',
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        verbose_name='Сообщество')
+        verbose_name='Сообщество',
+        related_name='posts'
+    )
 
     class Meta:
         ordering = ['-pub_date']
-        default_related_name = 'posts'
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import Post
+from .models import Group
 
 NUMBER_OF_LAST_POSTS = 10
 
 
 def index(request):
     posts = Post.objects.select_related('group').all()[:NUMBER_OF_LAST_POSTS]
-    title = 'Yatube'
     context = {
         'posts': posts,
-        'title': title
     }
     return render(request, 'posts/index.html', context)
 
@@ -17,10 +18,8 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:NUMBER_OF_LAST_POSTS]
-    title = 'Сообщества'
     context = {
         'posts': posts,
         'group': group,
-        'title': title
     }
     return render(request, 'posts/group_list.html', context)

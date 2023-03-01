a93d97a70db9981d110499cbb5b91c5f9d556ead
Ridesiberia <119219467+Ridesiberia@users.noreply.github.com>
2023-03-01 09:45:05 +0700

    исправленная версия после третьего ревью

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -21,20 +21,3 @@ def group_posts(request, slug):
         'group': group,
     }
     return render(request, 'posts/group_list.html', context)
-
-
-'''
-def index(request):
-    author = User.objects.get(username='leo')
-    keyword = "утро"
-    start_date = datetime.date(1854, 7, 7)
-    end_date = datetime.date(1854, 7, 21)
-    posts = Post.objects.filter(
-        text__contains=keyword
-    ).filter(
-        author=author
-    ).filter(
-        pub_date__range=(start_date, end_date)
-    )
-    return render(request, "index.html", {"posts": posts})
-'''
